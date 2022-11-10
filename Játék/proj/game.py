34ceb797e814eeba0c4842ab978a72de296edf66
Ványik Alex 9B <93581042+VanyikAlex@users.noreply.github.com>
2022-11-10 12:28:45 +0100

    First

---
+++ Játék/proj/game.py
@@ -0,0 +1,18 @@
+import pygame
+
+window = pygame.display.set_mode((600,600))
+pygame.display.set_caption("Amőba")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running == False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    window.fill((0, 0, 0))
+    window.blit(letterX, (0, 0))
+    pygame.display.flip()
